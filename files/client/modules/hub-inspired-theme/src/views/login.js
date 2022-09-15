9b3f7f77293e992b325d67f417435bf0cbf3ea9a
Jonathan Lindgren <jonathan@novastream.se>
2022-09-15 19:30:49 +0200

    CSS fixes
    Some table and modal content fixes. We have also added the missing record styles

--- files/client/modules/hub-inspired-theme/src/views/login.js
+++ files/client/modules/hub-inspired-theme/src/views/login.js
@@ -2,29 +2,23 @@ define('hub-inspired-theme:views/login', 'views/login', function (Dep) {
 
     return Dep.extend({
 
-        // specify your custom template
         template: 'hub-inspired-theme:login',
 
         setup: function () {
-            // calling parent setup method, you can omit it
             Dep.prototype.setup.call(this);
 
-            // add custom class to body during login
             $('body').addClass('hubinspired-login');
         },
         onRemove: function () {
-
-            // remove custom class from body
             $('body').removeClass('hubinspired-login');
         },		
 		afterRender: function () {
-			// calling parent setup method, you can omit it
             Dep.prototype.afterRender.call(this);
+
+			$('#field-userName').focus();
 			
-			// disable submit button
             $('#btn-login').attr('disabled', true);
-			
-			// username key down and change event
+						
 			$('#field-userName').on('keydown', function() {
 				if ($(this).val().length >= 2 && $('#field-password').val().length >= 2) {
 					$('#btn-login').attr('disabled', false);
@@ -38,8 +32,7 @@ define('hub-inspired-theme:views/login', 'views/login', function (Dep) {
 					$('#btn-login').attr('disabled', true);
 				}
 			});
-			
-			// password key down and change event
+						
 			$('#field-password').on('keydown', function() {
 				if ($(this).val().length >= 2 && $('#field-userName').val().length >= 2) {
 					$('#btn-login').attr('disabled', false);
@@ -53,8 +46,7 @@ define('hub-inspired-theme:views/login', 'views/login', function (Dep) {
 					$('#btn-login').attr('disabled', true);
 				}
 			});
-			
-			// hide and show password link click event
+						
 			$('#link-showhide-password').on('click', function(event) {
 				event.preventDefault();
 				
@@ -66,10 +58,7 @@ define('hub-inspired-theme:views/login', 'views/login', function (Dep) {
 					$('#field-password').attr('type', 'password');
 				}
 			});
-		 },
-
-        // include the custom values in the standard "data" function which will provide the placeholder values to the template
-        // the values for "logoSrc" and "showForgotPassword" are the standard values specified in the core login script
+		},
         data: function () {
             return{        
               logoSrc: this.getLogoSrc(),
